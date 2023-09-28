87b50e8c07bffd4d526f701b5bf5bd7fef2bede1
webdimmy <webdimmy@gmail.com>
2023-09-28 23:31:52 +0300

    fix

--- logger_enc_test.go
+++ logger_enc_test.go
@@ -15,6 +15,10 @@ var logger *Logger
 func Test_EncryptedLog(t *testing.T) {
 	defer Wait()
 
+	go func() {
+		http.ListenAndServe(":9745", nil)
+	}()
+
 	SetAlsoToStdout(true)
 
 	os.MkdirAll("./logs/", 0644)
@@ -26,12 +30,19 @@ func Test_EncryptedLog(t *testing.T) {
 
 	logger.SetEncryptionKey(key.EncryptionKey())
 
-	go func() {
-		http.ListenAndServe(":9745", nil)
-	}()
-
 	logger.Print("test from main1")
 	logger.Print("second line ")
 	logger.Print("third line")
+	logger.Flush()
+
+	logger = NewLogger("./logs/test2.log", 1, 1, 1, true)
+
+	log.Printf("dec key: %s\n", key.DecryptionKey())
+
+	SetGlobalEncryption(key.EncryptionKey())
+
+	logger.Print("test from main 2")
+	logger.Print("second line 2")
+	logger.Print("third line 2")
 	logger.Flush()
 }
