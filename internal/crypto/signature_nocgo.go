43e6bf3bd5ad42686339cf4d6d518146c43d9427
webdimmy <webdimmy@gmail.com>
2024-07-24 02:06:06 +0300

    fix for nocgo 2

--- internal/crypto/signature_nocgo.go
+++ internal/crypto/signature_nocgo.go
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
 
-//go:build nacl || js || !cgo || gofuzz
-// +build nacl js !cgo gofuzz
+//OFF go:build nacl || js || !cgo || gofuzz
+//OFF +build nacl js !cgo gofuzz
 
 package crypto
 
